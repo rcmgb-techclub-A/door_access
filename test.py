d6b8a107d45cf79b68e0f4671e75676fbe4e265f
unknown <kirstinwdwd@gmail.com>
2019-02-09 14:00:11 +0000

    added table display

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import sqlite3
+import string
 conn = sqlite3.connect("C:\Users\codeclub\door_access\door_access.db")
 conn.text_factory = str
 c = conn.cursor()
@@ -8,19 +9,24 @@ ac= ([r[0] for r in c.fetchall()])
 print("Tables")
 for col2 in ac:
     i= i+1
-    print ("%d.  %s") % (i , str(col2))
+    print ("%d  %s") % (i , str(col2))
 table_selected=int(raw_input("What table would you like to use?"))
 table_selected=table_selected-1
 table= ("SELECT * FROM %s") %(ac[table_selected])
 c.execute(table)
 bc=c.fetchall()
-g = 0
 for col2 in bc:
-    g= g+1
-    #print ("%d.  %s") % (g , str(col2[1]))
-    for h in range(1,col2.shape[1]):
-        dave = dave.col2[h]
-    print(dave)    
+    col2 = str(col2)
+    col2=string.replace(col2,")","")
+    col2=string.replace(col2,"(","")
+    col2=string.replace(col2,"\'","")
+    bob= col2.split(",")
+    
+    for p in range(0,len(bob)):
+        print (bob[p]),
+    print("")
+
         
 
 c.close()
+
